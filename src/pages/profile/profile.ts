92169aaa050a97884525f8244f42463350dc83f5
marcbitenl <marcbitenl@gmail.com>
2020-04-25 17:15:13 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -32,7 +32,14 @@ cliente : ClienteDTO;
         this.cliente = response;
         this.getImageIfExists();
       },
-      error =>{});
+      error =>{
+        if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+        }
+      });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
